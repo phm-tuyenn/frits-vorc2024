eaf6e12e7851055c30ee7fcfaab0add90a5eb1fd
DanhCaBa <nguyendanhkhai.work@gmail.com>
2023-05-31 14:33:40 +0700

    Co-authored-by: ChroniclesOfLife <ChroniclesOfLife@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,24 @@
 #define pressures false
 #define rumble false
 
+
+
+
+#define LeftServoGround 2
+#define RightServoGround 3
+#define LeftServoPulley 4
+#define RightServoPulley 5
+
+float timeOn = 2000;
+
+
 PS2X ps2x;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 byte error = -1;
 uint16_t tryNum = 0;
+byte doorState = 1;
+
 
 void setSpeed(int16_t left, int16_t right) {
     if(left >= 0) {
@@ -31,6 +44,10 @@ void setSpeed(int16_t left, int16_t right) {
         pwm.setPWM(10, 0, 0);
         pwm.setPWM(11, 0, -right);
     }
+}
+
+float servoConverter(float angle, float timeOn) {
+    return timeOn / (20 * (angle/180) / 4096);
 }
 
 void setup() {
@@ -52,6 +69,36 @@ void loop() {
     else if(ps2x.Button(PSB_PAD_DOWN)) setSpeed(-1023, -1023);
     else setSpeed(0, 0);
     
+    /*CƠ CHẾ HOẠT ĐỘNG CỦA 2 SERVO CỔNG: 
+    - Kênh 2 và kênh 3.
+    - Có 3 chế độ: đóng, mở vừa, mở rộng.
+    - Sử dụng 2 phím R1-R2 để luân chuyển giữa các chế độ.
+        với 1 - đóng | 2 - mở vừa | 3 - mở rộng
+    - R1 ++1 trạng thái
+    - R2 --1 trạng thái*/
+    
+    if ((doorState == 1 or doorState == 2) and ps2x.ButtonPressed(PSB_R2)) {
+        doorState++;
+    //      // Extend door outwards
+    //     pwm.writeMicroseconds(LeftServoGround, timeOn);
+    //  // Extend door outwards
+    //     pwm.writeMicroseconds(RightServoGround, timeOn);
+
+        pwm.setPWM(LeftServoGround, 0, 410); // chọn kênh servo số 5
+        pwm.setPWM(RightServoGround, 0, 410); // chọn kênh servo số 5
+    }
+    if ((doorState == 2 or doorState == 3) and ps2x.ButtonPressed(PSB_R1)) {
+        doorState--;
+        pwm.setPWM(LeftServoGround, 0, -410); // chọn kênh servo số 5
+        pwm.setPWM(RightServoGround, 0, -410); // chọn kênh servo số 5
+    }
+    /*CƠ CHẾ HOẠT ĐỘNG CỦA 2 SERVO TRÊN:
+    - Có 2 chế độ: nâng lên và hạ thấp xuống.
+    - Sử dụng 2 phím L1-L2 để luân chuyển giữa 3 chế độ.
+     với 1 - nâng lên | 2 - tạm dừng | 3 - hạ thấp
+    - L1 ++1 trạng thái
+    - R2 --1 trạng thái*/
+
     ps2x.read_gamepad();
     delay(10);
 }
